5524ef302ca086f0e29921b8449e7f286d94dade
saamirkc <samir.kc.a20@gmail.com>
2024-10-25 23:36:21 +0545

    Commiting token class

---
+++ src/main/java/com/samir/book_backend/token/Token.java
@@ -0,0 +1,32 @@
+package com.samir.book_backend.token;
+
+import com.samir.book_backend.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId",nullable = false)
+    private User user;
+
+
+}
