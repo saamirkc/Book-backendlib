f8d313f43e8152a7ac53e21671b0f6b7e76acd5b
saamirkc <samir.kc.a20@gmail.com>
2024-10-27 22:39:00 +0545

    Commit

---
+++ src/main/java/com/samir/book_backend/auth/AuthenticationRequest.java
@@ -0,0 +1,21 @@
+package com.samir.book_backend.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthenticationRequest {
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
+    private String email;
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8,message = "Password should be minimum 8 characters long")
+    private String password;
+}
